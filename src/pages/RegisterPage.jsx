686546bd6e9b67c6b3207c19bfeb9eb61c3978a5
Javier A. Fuchs <gamextar@hotmail.com>
2026-04-16 20:17:48 -0300

    FIX?

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -532,7 +532,7 @@ function FichaPreview({ data, currentStep, roleImages = {}, optimizeMotion = fal
                   transition={optimizeMotion ? { duration: 0.2, ease: 'easeOut' } : { type: 'spring', stiffness: 300 }}
                   className="flex flex-col items-center"
                 >
-                  <div className="flex min-h-[48px] items-end justify-center">
+                  <div className="flex min-h-[48px] items-end justify-center pt-1">
                     <div className="max-w-[240px] overflow-x-auto overflow-y-hidden overscroll-x-contain scrollbar-hide [-webkit-overflow-scrolling:touch]">
                       <div className="inline-flex min-w-max items-end justify-start gap-0.5 px-1">
                         {seekingArr.map((s, i) => (
