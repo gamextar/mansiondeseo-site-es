c934e1c5a89aac8bfaa996283ea86aa1373712fe
Javier A. Fuchs <gamextar@hotmail.com>
2026-04-16 09:22:50 -0300

    padding left stories rail 3

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1018,7 +1018,7 @@ export default function FeedPage({ initialData }) {
           <Radio className="w-4 h-4 text-mansion-crimson" />
           <p className="text-text-muted text-sm lg:text-base font-medium">Video Flashes</p>
         </div>
-        <div className="pl-[6px] pr-[12px] lg:px-0">
+        <div className="pl-[5px] pr-[12px] lg:px-0">
           <AnimatedBlock
             ref={storiesScrollRef}
             className={`flex overflow-x-auto scrollbar-hide pb-2 select-none ${desktopStoryRailEnhanced ? 'lg:cursor-grab active:lg:cursor-grabbing' : ''}`}
