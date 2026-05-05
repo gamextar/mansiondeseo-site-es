911e41943e11e2d871c471a73de66c2ceccf1ea2
Javier A. Fuchs <gamextar@hotmail.com>
2026-05-05 11:58:10 -0300

    Restore admin thumbnail batches to twenty

--- src/pages/admin/AdminUsersPage.jsx
+++ src/pages/admin/AdminUsersPage.jsx
@@ -7,9 +7,9 @@ import { getGalleryPhotoThumbnail } from '../../lib/profileMedia';
 import { optimizeAvatarThumbnailFromUrl, optimizeGalleryThumbnailFromUrl } from '../../lib/imageOptimize';
 
 const ADMIN_SELECTED_GALLERY_THUMB_BATCH_LIMIT = 20;
-const ADMIN_GALLERY_THUMB_USER_BATCH_LIMIT = 50;
-const ADMIN_AVATAR_THUMB_BATCH_LIMIT = 50;
-const ADMIN_USERS_PAGE_LIMIT = 50;
+const ADMIN_GALLERY_THUMB_USER_BATCH_LIMIT = 20;
+const ADMIN_AVATAR_THUMB_BATCH_LIMIT = 20;
+const ADMIN_USERS_PAGE_LIMIT = 20;
 const ADMIN_USERNAME_MAX_LENGTH = 18;
 
 function timeAgo(dateStr) {
